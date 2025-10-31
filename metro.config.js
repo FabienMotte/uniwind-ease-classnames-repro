584541a596d14fcad99d2228851eda358c24b3a5
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2025-10-31 21:07:28 +0900

    chore: move uniwind types to project root

--- metro.config.js
+++ metro.config.js
@@ -8,8 +8,5 @@ const config = getDefaultConfig(__dirname);
 module.exports = withUniwindConfig(config, {  
   // relative path to your global.css file (from previous step)
   cssEntryFile: './global.css',
-  // (optional) path where we gonna auto-generate typings
-  // defaults to project's root
-  dtsFile: './app/uniwind-types.d.ts'
 });
 
